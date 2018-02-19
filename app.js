40ee6ae803978c1ea26232d0e3b4fdc2a540d18f
Farid Wilhelm Zimmermann <farwilhelm@gmail.com>
2018-02-19 21:00:15 +0000

    Changed modular interface

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
-const request = require('./logic/request'),
+const Request = require('./logic/request'),
       data = require('./logic/dataHandling');
-      
-request.getData(process.argv[2], (err, res) => {
-    
-    if(err) {
-        console.log(`Error: ${err.message}! `)
-    }
-    
-    data.parseDataSet(res, process.argv[3])
-    
-    /*
-    try {
-        console.log(data.createOutputString(res));
-    } catch (e) {
-        if (e instanceof TypeError) {
-            console.log(`\nWrong or invalid symbol: \x1b[1m\x1b[31m${process.argv[2]}\u001b[0m. Please try again with a valid stock marker.\n\x1b[32mFor a full list of stock symbols visit http://eoddata.com/symbols.aspx\n`);
+
+const req = new Request(process.argv[2]); //instantiates request object
+
+if (!process.argv[3]) { //meaning that users requests default = latest stock data from closed trading day
+    req.makeRequest(null, null, null, (err, res) => {
+        if (err) console.log(`Error: ${err.message}! `)
+        try { //try/catch determines if stocksymbol was valid and a parsable response was returned
+            console.log(data.createOutputString(res));
+        } catch (e) {
+            if (e instanceof TypeError) {
+                console.log(`\nWrong or invalid symbol: \x1b[1m\x1b[31m${process.argv[2]}\u001b[0m. Please try again with a valid stock marker.\n\x1b[32mFor a full list of stock symbols visit http://eoddata.com/symbols.aspx\n`);
+            }
         }
-    }*/
+    })
+
+} else if(process.argv[3] === 'now' && [1,5,15,30,60].indexOf(parseInt(process.argv[4])) !== -1) {
+    //Case that User wants interday data and has supplied a valid time interval for the two consecutive data points
+    req.makeRequest(true, process.argv[4], null, (err, res) => {
+        if (err) console.log(`Error: ${err.message}! `)
+        try { 
+            console.log(res)
+        } catch (e) {
+            if (e instanceof TypeError) {
+                console.log(`\nWrong or invalid symbol: \x1b[1m\x1b[31m${process.argv[2]}\u001b[0m. Please try again with a valid stock marker.\n\x1b[32mFor a full list of stock symbols visit http://eoddata.com/symbols.aspx\n`);
+            }
+        }
+    })
+} else if(process.argv[3]) {
     
-});
+}
--- logic/request.js
+++ logic/request.js
@@ -4,40 +4,45 @@ const https = require('https'),
 
 dotenv.config();
 
-/*function printHelp() {
-    //Prints usage information
-    console.log("USAGE: node app.js STOCKSYMBOL ");
-}*/
-
-function makeRequest(sym, callback) {
-    //Makes the API request
-    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${sym}&apikey=${process.env.API_KEY}&outputsize=full`;
-    console.log(url)
-    const request = https.get(url, res => {
-        
-        if (res.statusCode === 200) { 
-            let output = "";
-            res.on('data', chunk => {
-                output += chunk.toString();
-            })
-            
-            res.on('error', callback);
-            
-            res.on("end", () => {
-                output = JSON.parse(output);
-                callback(null, output);
-            })
-        } else {
-            let error = new Error(`Error: ${http.STATUS_CODES[res.statusCode]}`)
-            return callback(error);
-        }
-    }).on('error', e => {
-        console.log('Invalid request!');
-    })
+class Request{
     
+    constructor(sym) {
+        this.sym = sym;
+    }
+    
+    makeRequest(now = null, interval = null, specificDate = null, callback) {
+        //Makes api request, based on parameters defined, if specificDate not defined just gets latest stock data,
+        //otherwise gets specified date, if now is defined get real time intraday  data(in interval defined)
+        let url = `https://www.alphavantage.co/query?function=${now === null ? 'TIME_SERIES_DAILY' : 'TIME_SERIES_INTRADAY'}&symbol=${this.sym}${interval !== null ? '&interval=' + interval + 'min' : ''}&apikey=${process.env.API_KEY}`;
+        url += specificDate !== null ? '&outputsize=full' : '';
+        const request = https.get(url, res => {
+            if (res.statusCode === 200) { 
+                let output = "";
+                res.on('data', chunk => {
+                    output += chunk.toString();
+                })
+                res.on('error', callback);
+                res.on("end", () => {
+                    output = JSON.parse(output);
+                    callback(null, output);
+                })
+            } else {
+                let error = new Error(`Error: ${http.STATUS_CODES[res.statusCode]}`)
+                return callback(error);
+             }
+        }).on('error', e => {
+            console.log('Invalid request!');
+        })   
+    }
+    
+    verifyDate(date) {
+        return /^(\d\d\d\d) - 
+    }
 }
 
 
 
 
-module.exports.getData = makeRequest;
+
+
+module.exports = Request;
