023cb76b5cc0899070e0ac7234e614044e9fb598
Farid Wilhelm Zimmermann <farwilhelm@gmail.com>
2018-02-21 15:39:39 +0000

    Improved date verify

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
-const Request = require('./logic/request'),
-      data = require('./logic/dataHandling');
-
+const Request = require('./logic/request');
 const req = new Request(process.argv[2]); //instantiates request object
 
 if (!process.argv[3]) { //meaning that users requests default = latest stock data from closed trading day
     req.makeRequest(null, null, null, (err, res) => {
-        if (err) console.log(`Error: ${err.message}! `)
-        try { //try/catch determines if stocksymbol was valid and a parsable response was returned
-            console.log(data.createOutputString(res));
-        } catch (e) {
-            if (e instanceof TypeError) {
-                console.log(`\nWrong or invalid symbol: \x1b[1m\x1b[31m${process.argv[2]}\u001b[0m. Please try again with a valid stock marker.\n\x1b[32mFor a full list of stock symbols visit http://eoddata.com/symbols.aspx\n`);
-            }
-        }
+        req.checkRequest(err, res, null, false)
     })
 
 } else if(process.argv[3] === 'now' && [1,5,15,30,60].indexOf(parseInt(process.argv[4])) !== -1) {
     //Case that User wants interday data and has supplied a valid time interval for the two consecutive data points
     req.makeRequest(true, process.argv[4], null, (err, res) => {
-        if (err) console.log(`Error: ${err.message}! `)
-        try { 
-            console.log(res)
-        } catch (e) {
-            if (e instanceof TypeError) {
-                console.log(`\nWrong or invalid symbol: \x1b[1m\x1b[31m${process.argv[2]}\u001b[0m. Please try again with a valid stock marker.\n\x1b[32mFor a full list of stock symbols visit http://eoddata.com/symbols.aspx\n`);
-            }
-        }
+       req.checkRequest(err, res, null, true);
     })
-} else if(process.argv[3]) {
-    
+} else if(req.verifyDate(process.argv[3])) {
+    req.makeRequest(null, null, process.argv[3], (err, res) => {
+        req.checkRequest(err, res, process.argv[3], false)
+  
+    })
+} else {
+    console.log("Invalid argument list or date format. Check documentation for how to properly input arguments")
 }
--- logic/request.js
+++ logic/request.js
@@ -10,11 +10,13 @@ class Request{
         this.sym = sym;
     }
     
+
     makeRequest(now = null, interval = null, specificDate = null, callback) {
         //Makes api request, based on parameters defined, if specificDate not defined just gets latest stock data,
         //otherwise gets specified date, if now is defined get real time intraday  data(in interval defined)
         let url = `https://www.alphavantage.co/query?function=${now === null ? 'TIME_SERIES_DAILY' : 'TIME_SERIES_INTRADAY'}&symbol=${this.sym}${interval !== null ? '&interval=' + interval + 'min' : ''}&apikey=${process.env.API_KEY}`;
         url += specificDate !== null ? '&outputsize=full' : '';
+        console.log(url)
         const request = https.get(url, res => {
             if (res.statusCode === 200) { 
                 let output = "";
@@ -35,8 +37,51 @@ class Request{
         })   
     }
     
+    checkRequest(err, response, date, intra) {
+         if (err) console.log(`Request or Server Error: ${err.message}! `)
+        try { 
+            if(!intra) {
+                console.log(this.createOutputRegular(response, date));
+            } else {
+                this.createOutputIntra(response);
+            }
+            
+        } catch (e) {
+            if (e instanceof TypeError) {
+                console.log(`\nWrong or invalid symbol: \x1b[1m\x1b[31m${this.sym}\u001b[0m. Please try again with a valid stock marker.\n\x1b[32mFor a full list of stock symbols visit http://eoddata.com/symbols.aspx\n`);
+            }
+        }
+    }
+    
+    createOutputRegular(res, date) {
+            const d = date !== null ? res['Time Series (Daily)'][date.toString()] : Object.keys(res['Time Series (Daily)'])[0];
+            console.log(d)
+            return `
+            \x1b[34m\x1b[1mInformation for "${res['Meta Data']['2. Symbol']}" on ${d}:\x1b[0m
+                Open: ${res['Time Series (Daily)'][d]['1. open']}
+                Close: ${res['Time Series (Daily)'][d]['4. close']}
+                High: ${res['Time Series (Daily)'][d]['2. high']} 
+                Low: ${res['Time Series (Daily)'][d]['3. low']}
+                Volume: ${res['Time Series (Daily)'][d]['5. volume']}
+                `;
+}
+
+
+    createOutputIntra(res, interval) {
+        console.log(res)
+    }
+
     verifyDate(date) {
-        return /^(\d\d\d\d) - 
+        try {
+            let dateArr = date.split("-");
+            let correctYear = dateArr[0] >= new Date().getFullYear() - 10;;
+            let correctMonth = dateArr[1] > 0 && dateArr[1] <= 12;
+            let correctDay = dateArr[2] > 0 && dateArr[2] <= 31;
+            return /^(\d\d\d\d)-(\d\d)-(\d\d)/.test(date) &&  correctYear && correctMonth && correctDay;
+        } catch(e) {
+            return false;
+        }
+        
     }
 }
 
